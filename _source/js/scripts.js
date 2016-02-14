0b917ece49945e85750895db94a37e4c049bcdc2
Derek Moon <dmoon79@gmail.com>
2016-02-13 17:28:25 -0800

    header work

--- _source/js/scripts.js
+++ _source/js/scripts.js
@@ -7,22 +7,36 @@
   $(document).ready(function() {
 
     // var divCount = $('.skillbar').length;
-    // console.log(divCount);
+    // // console.log(divCount);
+    // var skillBox = $('.skillbar');
     // var skillValue = $('.skillbar').attr('data-percent');
     // var skillValue = parseInt(skillValue);
-    // switch(skillValue){
-    //   case (skillValue <= 59): alert("Lower than 60");
-    //   break;
-    //   case (skillValue >= 60): alert("More than than 60");
-    //   break;
-    // }
-    // console.log(typeof skillValue);
+    // var i = 0;
+    $('.skillbar')
+      .find('.skill-bar-percent')
+      .css( "color", "green" );
+    // {
+    //   switch (x) {
+    //     case (skillValue < 59):
+    //       $(this).html('Test');
+    //       console.log(i + " Lower than 60");
+    //       break;
+    //     case (skillValue > 60): 
+    //       $(this).html("High");
+    //       console.log(i + " Lower than 60");
+    //       break;
+    //     default:
+    //       $(this).html("Cant Find it");
+    //       console.log("is this working");
+    //       break;
+    //   }
+    // });
     
     $('.flexslider').flexslider({
       animation: "slide"
     });
 
-    $('.job__content').hide();
+    // $('.job__content').hide();
 
     $('.skillbar').skillBars({
       from: 0,
