d721a2372e3257cfa4f50d623855b6e36c663aea
Derek Moon <dmoon79@gmail.com>
2017-05-17 11:07:38 -0700

    update JS skills

--- _source/js/scripts.js
+++ _source/js/scripts.js
@@ -6,44 +6,9 @@
   'use strict';
   $(document).ready(function() {
 
-    // var divCount = $('.skillbar').length;
-    // // console.log(divCount);
-    // var skillBox = $('.skillbar');
-    // var skillValue = $('.skillbar').attr('data-percent');
-    // var skillValue = parseInt(skillValue);
-    // var i = 0;
-    $('.skillbar')
-      .find('.skill-bar-percent')
-      // .css( "color", "white" );
-    // {
-    //   switch (x) {
-    //     case (skillValue < 59):
-    //       $(this).html('Test');
-    //       console.log(i + " Lower than 60");
-    //       break;
-    //     case (skillValue > 60):
-    //       $(this).html("High");
-    //       console.log(i + " Lower than 60");
-    //       break;
-    //     default:
-    //       $(this).html("Cant Find it");
-    //       console.log("is this working");
-    //       break;
-    //   }
-    // });
-
     $('.flexslider').flexslider({
       animation: "slide"
     });
-
-    // $('.job__content').hide();
-
-    // $('.job__expand').on( 'click' , function(){
-    //   $(this).text($(this).text() == 'Show Details +' ? 'Hide Details -' : 'Show Details +');
-    //   $(this).next('.job__content').toggle("slow", function(){
-    //     $(this).prev('.button').css("background-color", "#C31010");
-    //   });
-    // });
 
     $('.skillbar').skillBars({
       from: 0,
